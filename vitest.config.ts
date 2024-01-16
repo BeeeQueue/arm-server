2b6490879366822442b067987663efd091275f7d
Adam <adam@haglund.dev>
2024-01-16 02:37:52 +0100

    up deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig(async ({ command }) => ({
   resolve: {
-    alias: { "@": "./src" },
+    alias: [{ find: "@", replacement: "/src" }],
   },
 
   test: {
